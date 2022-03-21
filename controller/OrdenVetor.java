07245973f64049f3c762580813c30d6401e29327
Kennedy Santos <oliveirakennedysantos9@gmail.com>
2022-03-20 21:11:37 -0300

    2 commit

--- controller/OrdenVetor.java
+++ controller/OrdenVetor.java
@@ -36,27 +36,55 @@ public class OrdenVetor {
 
 
 		}
+		
+			public static void bubbleSort(int[] vetor) {
+				boolean troca = true;
+				int x;
+				while (troca) {
+					troca = false;
+					for (int i = 0 ; i < vetor.length - 1; i++) {
+						if(vetor[i]> vetor[i+1]) {
+							aux = vetor[i];
+							vetor[i] = vetor[i+1];
+							vetor[i+1] = aux;
+							troca = true;
+						}
+					}
+				}
+				
+			}
+		
+		public static void quickSort(int[] vetor) {
+			quickSort(vetor, 0, vetor.length - 1);
+			}
 
-		public static int[] criarVetor(int tamanhoVetor) {
+			private static void quickSort(int[] vetor, int inicio, int fim) {
+			if(inicio < fim) {
+			int posicaoPivo = separar(vetor, inicio, fim);
+			quickSort(vetor, inicio, posicaoPivo - 1);
+			quickSort(vetor, posicaoPivo + 1, fim);
+			}
+			}
+		
+			private static int separar(int[] vetor, int inicio, int fim) {
+				int pivo = vetor[inicio];
+				int i = inicio +1 , f = fim;
+				while(i <= f) {
+				if(vetor[i] <= pivo)
+				i++;
+				else if (pivo < vetor[f])
+				f--;
+				else {
+				int troca = vetor[i];
+				vetor[i] = vetor[f];
+				vetor[f] = troca;
+				i++;
+				f--;
+				}
 
-		Random random = new Random();
-		int vetor[] = new int[tamanhoVetor];
-		for (int i = 0; i < tamanhoVetor; i++) {
-		vetor[i] = random.nextInt(100);
-		}
-		return vetor;
-		}
-
-
-
-		@SuppressWarnings("rawtypes")
-		public static Comparable[] convertToComparable(int[] vet) {
-		Integer[] vetComp = new Integer[vet.length];
-		int x = 0;
-		for (int i : vet) {
-		vetComp[x] = i;
-		x++;
-		}
-		return vetComp;
-		}
+				}
+				vetor[inicio] = vetor[f];
+				vetor[f] = pivo;
+				return f;
+				}
 }
